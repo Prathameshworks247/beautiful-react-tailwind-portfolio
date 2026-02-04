71fb86ccc83f35071d7d644c11990e03f5839df5
Prathameshworks247 <prathameshworks247@gmail.com>
2026-02-05 01:34:49 +0530

    favicon change

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="public/prathamesh__.pdf"
+                href="https://drive.google.com/file/d/1D-BXAEuQetHSMkGS_EmLwEsYg8Ntj-bR/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 target="_blank"
                 rel="noopener noreferrer"
